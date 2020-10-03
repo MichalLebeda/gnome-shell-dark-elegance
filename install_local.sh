61a3e54fcb3a57e024d67ddbdd43f6109463196f
Michal Lebeda <MichalLebeda@users.noreply.github.com>
2020-10-03 11:22:41 +0200

    refresh extension to apply selected theme

--- install_local.sh
+++ install_local.sh
@@ -3,3 +3,5 @@
 rm -rf ~/.themes/adwaita-dark-elegance
 mkdir -p ~/.themes
 cp -r dist ~/.themes/adwaita-dark-elegance
+gnome-extensions disable user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
